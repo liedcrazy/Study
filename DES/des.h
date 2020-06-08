b46da5c1399fa486b2e421662ab032feb30deb32
Denis <35600639+liedcrazy@users.noreply.github.com>
2020-06-09 01:46:03 +0300

    Initial commit

---
+++ DES/des.h
@@ -0,0 +1,70 @@
+#ifndef DES_H
+#define DES_H
+
+/*
+class Des
+{
+public:
+    Des();
+};
+*/
+/*  DES Encryption & Decryption Code */
+
+# include <stdio.h>
+# include <fstream>
+# include <string.h>
+# include <conio.h>
+# include <iostream>
+# include <cstring>
+
+
+
+class Des
+{
+public:
+int keyi[16][48], // ключ для каждого раунда
+total[64],
+left[32],
+right[32],
+ck[28], // С и D по 28 бит каждый
+dk[28], //
+expansion[48],
+z[48],
+xor1[48],
+sub[32],
+p[32],
+xor2[32],
+temp[64],
+pc1[56],
+ip[64],
+inv[8][8];
+
+int key[64]={
+0,0,0,1,0,0,1,1,
+0,0,1,1,0,1,0,0,
+0,1,0,1,0,1,1,1,
+0,1,1,1,1,0,0,1,
+1,0,0,1,1,0,1,1,
+1,0,1,1,1,1,0,0,
+1,1,0,1,1,1,1,1,
+1,1,1,1,0,0,0,1
+};
+
+char final[1000];
+void IP();
+void PermChoice1();
+void PermChoice2();
+void Expansion();
+void inverse();
+void xor_two();
+void xor_oneE(int);
+void xor_oneD(int);
+void substitution();//int newParameter = 14
+void permutation();
+void keygen();
+char * Encrypt(char *);
+char * Decrypt(char *);
+};
+
+
+#endif // DES_H
---
+++ DES/main.cpp
@@ -0,0 +1,33 @@
+#include <QCoreApplication>
+#include <des.h>
+
+using namespace std;
+
+void clrscr()
+{
+    for(int i = 0; i < 50; i++)
+        cout<<endl;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Des d1,d2;
+    char *str=new char[1000];
+    char *str1=new char[1000];
+    clrscr(); //чистка экрана
+    //strcpy(str,"PHOENIX it & ece solutions.");
+    cout<<"Enter a string : ";
+    gets(str);
+    str1=d1.Encrypt(str);
+    cout<<"\ni/p Text: "<<str<<endl;
+    cout<<"\nCypher  : "<<str1<<endl;
+    //	 ofstream fout("out2_fil.txt"); fout<<str1; fout.close();
+    cout<<"\no/p Text: "<<d2.Decrypt(str1)<<endl;
+    //getch();
+
+    return a.exec();
+}
+
+
